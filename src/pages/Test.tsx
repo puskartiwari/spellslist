bf9b9b8fae39d82bd36dd0029d4a348f5667285e
puku tiwari <puskartiwari146@gmail.com>
2023-08-07 22:01:18 +0545

    reirect to spell page

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,71 +1,6 @@
-// import React, { useEffect, useState } from 'react';
-// import { useSearchParams } from 'react-router-dom';
-// import axios from 'axios';
-
-// export interface ITestPageProps {}
-
-// interface Spell {
-//     name: string;
-
-//     desc: string[] | undefined; // Make desc optional by allowing undefined
-//     // other properties of the spell
-// }
-// type test = {
-//     desc: string;
-// };
-
-// const TestPage: React.FunctionComponent<ITestPageProps> = (props) => {
-//     const [searchParams, setSearchParams] = useSearchParams();
-//     const [data, setData] = useState<string | null>(null);
-//     // const [posts, setPosts] = useState([] as any[]);
-
-//     // useEffect(() => {
-//     //     // fetchSpells();
-//     //     axios
-//     //         .get('http://www.dnd5eapi.co/api/spells/acid-arrow')
-//     //         .then((res) => {
-//     //             console.log(res);
-//     //             setPosts(res.data);
-//     //         })
-//     //         .catch((err) => {
-//     //             console.log(err);
-//     //         });
-//     // }, []);
-
-//     const fetchData = async () => {
-//         try {
-//             const response = await fetch(`http://www.dnd5eapi.co/api/spells/acid-arrow`);
-//             if (!response.ok) {
-//                 throw new Error('Failed to fetch spells');
-//             }
-//             const jsonData = await response.json();
-//             setData(JSON.stringify(jsonData, null, 1));
-//             // setData(JSON.stringify(jsonData, null, 2));
-//         } catch (error) {
-//             console.error('Error fetching spells:', error);
-//         }
-//     };
-
-//     const food = searchParams.get('food');
-//     const dog = searchParams.get('dog');
-
-//     return (
-//         <div>
-//             <p>This is the test page.</p>
-//             {food && <p>Favorite food is: {food}</p>}
-//             {dog && <p>My dogs name is: {dog}</p>}
-//             <button onClick={() => setSearchParams({ food: 'chicken', dog: 'kyle' })}>Change up the Params!</button>
-//             <h1>Spells List</h1>
-//             <button onClick={fetchData}>Fetch Data</button>
-//             {data && <pre>{data}</pre>}
-//         </div>
-//     );
-// };
-
-// export default TestPage;
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 interface Spell {
     desc: string[];
@@ -87,6 +22,11 @@ const TestPage: React.FC = () => {
 
     return (
         <div>
+            <p>
+                <Link to="/about" className="spell-lists">
+                    Go to Spell Page
+                </Link>
+            </p>
             <h1>Acid Arrow Spell</h1>
 
             {spellData?.desc.map((description, index) => (
